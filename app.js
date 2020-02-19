6091932e562ffa6aa06b4947a6b7ffcd7e0d203d
abhinav-TB <abhinavtb@gmail.com>
2020-02-19 19:43:22 +0530

    aded post request to form

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express =require('express');
+var bodyparser=require('body-parser');
 var app = express();
+var urlencodedParser = bodyparser.urlencoded({ extended: false });
 app.set('view engine','ejs');
 app.get('/',function(req,res){
     res.render('index');
@@ -7,6 +9,10 @@ app.get('/',function(req,res){
 app.get('/contact',function(req,res){
     
     res.render('contact',{qs:req.query});
+});
+app.post('/contact', urlencodedParser,function(req,res){
+    console.log(req.body);
+    res.render('contact-success',{data:req.body})
 });
 // app.get('/profile/:id',function(req,res){
 //     res.send('you request to see a profile with theid of'+req.params.id);
